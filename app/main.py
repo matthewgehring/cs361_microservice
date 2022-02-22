91a608be94b046c5f05b8c1db6b2e7511a8e59f7
unknown <mgehring9@gmail.com>
2022-02-22 11:27:29 -0800

    add api and image endpoints

--- app/main.py
+++ app/main.py
@@ -4,19 +4,29 @@ from wptools import page
 def get_meta(title):
     soup = page(title).get_parse()
     infobox = soup.data['infobox']
-    return soup
+    return infobox
+
+def get_images(title):
+    my_page = page(title).get_parse()
+    return my_page.data["image"][0]["url"]
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def welcome():
-    return "<h1>Welcome to my service. POST to /api to use. Contact Matthew with quesitons.</h1>"
+    return "<h1>Welcome to my service. POST to /api to use. Contact Matthew with questions.</h1>"
 
 @app.route('/api', methods=['POST'])
 def query_records():
     title = request.json
     meta = get_meta(title['title'])
     return jsonify(meta)
+
+@app.route('/image', methods=['POST'])
+def images():
+    title = request.json
+    images = get_images(title['title'])
+    return jsonify(images)
 
 # @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
 # def add_message(uuid):
